83fb1833c912bd86e62f0fe8ca9c8b210749bbe7
Martin Bechayda <martin.frederico29@gmail.com>
2025-08-07 17:35:07 +0800

    first commit

---
+++ Act1.dart
@@ -0,0 +1,12 @@
+void main() {
+  String greetings = 'Welcome';
+  dynamic user = 'Alice';
+  
+  print("$greetings, $user!");
+  
+  user = 25;
+  
+  print("$greetings, $user!");
+
+// you cant execute output when you use a dynamic variable outside the string. so you must use $variable_name inside the string for it to execute.
+}
